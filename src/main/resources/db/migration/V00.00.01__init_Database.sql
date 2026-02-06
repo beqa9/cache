d99717821c39f0349ebb45743c4959d304d225ce
beqa <beqabeqa329@gmail.com>
2026-02-06 12:51:19 +0400

    Initialize database schema with countries, cities, municipalities and products

--- src/main/resources/db/migration/V00.00.01__init_Database.sql
+++ src/main/resources/db/migration/V00.00.01__init_Database.sql
@@ -7,3 +7,28 @@ CREATE TABLE products (
     stock_quantity INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE countries (
+                             id BIGSERIAL PRIMARY KEY,
+                             name VARCHAR(100) NOT NULL UNIQUE,
+                             code VARCHAR(10) NOT NULL UNIQUE
+  );
+
+CREATE TABLE cities (
+                        id BIGSERIAL PRIMARY KEY,
+                        name VARCHAR(100) NOT NULL,
+                        country_id BIGINT NOT NULL,
+                        CONSTRAINT fk_city_country
+                            FOREIGN KEY (country_id)
+                                REFERENCES countries (id)
+                                ON DELETE CASCADE
+);
+
+CREATE TABLE municipalities (
+                                id BIGSERIAL PRIMARY KEY,
+                                name VARCHAR(100) NOT NULL,
+                                city_id BIGINT NOT NULL,
+                                CONSTRAINT fk_municipality_city
+                                    FOREIGN KEY (city_id)
+                                        REFERENCES cities (id)
+                                        ON DELETE CASCADE
+);
